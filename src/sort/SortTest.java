75b6b63a55328af2b0e27945a57135aed7695f0b
Yevhen V <wik@bdhr.co>
2018-12-18 11:41:11 +0000

    Revert to sorting 3 items only The point where quick sort pattern start to emerge.

--- src/sort/SortTest.java
+++ src/sort/SortTest.java
@@ -20,16 +20,6 @@ public class SortTest {
     return Arrays.asList(ints);
   }
 
-  private void sortBigList(int n) {
-    List<Integer> unsorted = new ArrayList<Integer>();
-    for (int i = 0; i < n; i++)
-      unsorted.add((int) (Math.random() * 10000.0));
-    List<Integer> sorted = sort(unsorted);
-//    sort(sorted);
-    for (int i = 0; i < n - 1; i++)
-      assertTrue(sorted.get(i) <= sorted.get(i + 1));
-  }
-
   @Test
   public void sortings() throws Exception {
     assertSorted(intList(), intList());
@@ -37,31 +27,33 @@ public class SortTest {
     assertSorted(intList(2, 1), intList(1, 2));
     assertSorted(intList(2, 1, 3), intList(1, 2, 3));
     assertSorted(intList(2, 3, 1), intList(1, 2, 3));
-    assertSorted(intList(1, 3, 2), intList(1, 2, 3));
-    assertSorted(intList(3, 2, 1), intList(1, 2, 3));
-    assertSorted(intList(3, 2, 2, 1), intList(1, 2, 2, 3));
-    sortBigList(50000);
   }
 
   private List<Integer> sort(List<Integer> list) {
     List<Integer> sorted = new ArrayList<Integer>();
-    if (list.size() == 0)
-      return list;
-    else {
-      List<Integer> l = new ArrayList<Integer>();
-      Integer m = list.get(0);
-      List<Integer> h = new ArrayList<Integer>();
-      for (int i : list.subList(1, list.size())) {
-        if (i > m)
-          h.add(i);
-        else
-          l.add(i);
-      }
 
-      sorted.addAll(sort(l));
-      sorted.add(m);
-      sorted.addAll(sort(h));
+    if (list.size() <= 1) {
+      return list;
+    } else if (list.size() == 2) {
+      if (list.get(0) > list.get(1)) {
+        sorted.add(list.get(1));
+        sorted.add(list.get(0));
+      } else {
+        sorted.add(list.get(0));
+        sorted.add(list.get(1));
+      }
+    } else if (list.size() == 3) {
+      if (list.get(1) > list.get(2)) {
+        sorted.add(list.get(2));
+        sorted.add(list.get(0));
+        sorted.add(list.get(1));
+      } else {
+        sorted.add(list.get(1));
+        sorted.add(list.get(0));
+        sorted.add(list.get(2));
+      }
     }
+
     return sorted;
   }
 }
