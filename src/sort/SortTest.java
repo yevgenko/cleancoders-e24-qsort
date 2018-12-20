474e699d4bc88003e50d12e14f36a0c6a15a4a2d
Yevhen V <wik@bdhr.co>
2018-12-20 16:22:46 +0000

    Revert to empty list

--- src/sort/SortTest.java
+++ src/sort/SortTest.java
@@ -23,37 +23,9 @@ public class SortTest {
   @Test
   public void sortings() throws Exception {
     assertSorted(intList(), intList());
-    assertSorted(intList(1), intList(1));
-    assertSorted(intList(2, 1), intList(1, 2));
-    assertSorted(intList(2, 1, 3), intList(1, 2, 3));
-    assertSorted(intList(2, 3, 1), intList(1, 2, 3));
   }
 
   private List<Integer> sort(List<Integer> list) {
-    List<Integer> sorted = new ArrayList<Integer>();
-
-    if (list.size() <= 1) {
-      return list;
-    } else if (list.size() == 2) {
-      if (list.get(0) > list.get(1)) {
-        sorted.add(list.get(1));
-        sorted.add(list.get(0));
-      } else {
-        sorted.add(list.get(0));
-        sorted.add(list.get(1));
-      }
-    } else if (list.size() == 3) {
-      if (list.get(1) > list.get(2)) {
-        sorted.add(list.get(2));
-        sorted.add(list.get(0));
-        sorted.add(list.get(1));
-      } else {
-        sorted.add(list.get(1));
-        sorted.add(list.get(0));
-        sorted.add(list.get(2));
-      }
-    }
-
-    return sorted;
+    return new ArrayList<Integer>();
   }
 }
